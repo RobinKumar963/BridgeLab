3c34aede1ab04212aba1a95dd036a04798370e23
RobinKumar963 <robbhood123@gmail.com>
2019-10-24 19:25:39 +0530

    Updated design pattern

---
+++ Annotation/MyAttributes.cs
@@ -0,0 +1,44 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=MyAttributes.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="Robin Kumar"/>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bridgelabz.DesignPattern.Annotation
+{
+    [AttributeUsage(AttributeTargets.All)]
+    class MyAttributes : Attribute
+    {
+
+        // Provides name of the member 
+        private string name;
+
+        // Provides description of the member 
+        private string action;
+
+        // Constructor 
+        public MyAttributes(string name, string action)
+        {
+            this.name = name;
+            this.action = action;
+        }
+
+        // property to get name 
+        public string Name
+        {
+            get { return name; }
+        }
+
+        // property to get description 
+        public string Action
+        {
+            get { return action; }
+        }
+    }
+}
---
+++ Annotation/Student.cs
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file=Student.cs" company="Bridgelabz">
+//   Copyright © 2019 Company="BridgeLabz"
+// </copyright>
+// <creator name="Robin Kumar"/>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+namespace Bridgelabz.DesignPattern.Annotation
+{
+    class Student
+    {
+        // Private fields of class Student 
+        private int rollNo; 
+        private string stuName; 
+        private double marks; 
+  
+        // The attribute MyAttribute is applied  
+        // to methods of class Student 
+        // Providing details of their utility 
+        [MyAttributes("Modifier", "Assigns the Student Details")] 
+        public void setDetails(int r, string sn, double m) 
+        { 
+            rollNo = r; 
+            stuName = sn; 
+            marks = m; 
+        } 
+  
+        [MyAttributes("Accessor", "Returns Value of rollNo")] 
+        public int getRollNo() 
+        { 
+            return rollNo; 
+        } 
+  
+        [MyAttributes("Accessor", "Returns Value of stuName")] public string getStuName() 
+        { 
+            return stuName; 
+        } 
+  
+        [MyAttributes("Accessor", "Returns Value of marks")] public double getMarks() 
+        { 
+            return marks; 
+        } 
+    }
+}
