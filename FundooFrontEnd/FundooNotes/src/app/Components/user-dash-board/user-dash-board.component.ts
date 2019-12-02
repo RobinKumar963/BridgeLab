b4a9721b44cbed2ed027d8ed4becabdff4048754
RobinKumar963 <robbhood123@gmail.com>
2019-12-02 11:11:05 +0530

    [ROBIN] Refactor FundooFrontEnd

--- FundooFrontEnd/FundooNotes/src/app/Components/user-dash-board/user-dash-board.component.ts
+++ FundooFrontEnd/FundooNotes/src/app/Components/user-dash-board/user-dash-board.component.ts
@@ -3,21 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-user-dash-board',
   templateUrl: './user-dash-board.component.html',
-  styleUrls: ['./user-dash-board.component.scss']
+  styleUrls: ['./user-dash-board.component.scss'
+]
 })
 export class UserDashBoardComponent implements OnInit {
 
   constructor() { }
   HideMenu()
   {
-    alert("Ok");
-    
+    //alert("Ok");
+
+        
     var x = document.getElementById("sidebar");
     if (x.style.display === "none") {
     x.style.display = "block";
     } else {
     x.style.display = "none";
     }
+
+
+ 
+
+
+ 
   }
 
   ngOnInit() {
--- FundooFrontEnd/FundooNotes/src/app/Components/user-sign-up/user-sign-up.component.ts
+++ FundooFrontEnd/FundooNotes/src/app/Components/user-sign-up/user-sign-up.component.ts
@@ -28,6 +28,27 @@ export class UserSignUpComponent implements OnInit {
       }, false);
       });
       }, false);
+
+
+
+
+
+
+      var user=
+      {
+        "useremail":((document.getElementById("validationCustom01") as HTMLInputElement).value),
+        "userid":"null",
+        "UserName":((document.getElementById("validationCustomUsername") as HTMLInputElement).value),
+        "Password":((document.getElementById("validationCustom02") as HTMLInputElement).value),
+        "CardType":((document.getElementById("validationCustom03") as HTMLInputElement).value)
+      }
+  
+      console.log( user );
+  
+      this.service.UserSignUP('Register',user).subscribe(data=>
+        {
+          console.log(data);
+        });
       
   }
 
