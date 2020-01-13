4c576d8f7c09c408d4c637d139f4c18c1d5e1400
RobinKumar963 <robbhood123@gmail.com>
2020-01-13 18:04:47 +0530

    [RobinKumar] Refactor FundooBackEnd

--- FundooBackEnd/Common/Helper/SendResetPasswordEmail.cs
+++ FundooBackEnd/Common/Helper/SendResetPasswordEmail.cs
@@ -39,7 +39,7 @@ namespace Common.Helper
             smtpClient.Credentials = new System.Net.NetworkCredential()
             {
                 UserName = "robbhood123@gmail.com",
-                Password = "iamsonofgodp12ocb2slim9012"
+                Password = ""
             };
             smtpClient.EnableSsl = true;
             smtpClient.Send(mailMessage);
